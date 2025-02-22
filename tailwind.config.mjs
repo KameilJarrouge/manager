1bf60ddfb3f996b9f0b83045cc81fd8f0df66000
Kamil Jarrouj <kamil@localhost.localdomain>
2025-02-22 19:54:14 +0200

    configured some colors in tailwind and cleared the default page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: "#222831",
+        primary: "#393E46",
+        foreground: "#EEEEEE",
+        accent: "#D65A31",
+        input_bg: "#4B4F58",
+        input_prefix_bg: "#3F424D",
       },
     },
   },
